cc10459e111886a9d7d69c698a5ee347f7abdbbf
Ryan Webb <webbryan92@gmail.com>
2019-01-27 10:57:18 -0500

    search function

--- Build an Address Book in Ruby/address_book.rb
+++ Build an Address Book in Ruby/address_book.rb
@@ -1,29 +1,87 @@
 require "./contact"
 
 class AddressBook
-    attr_reader :contacts
-
-    def initialize
-        @contacts = []
+  attr_reader :contacts
+  
+  def initialize
+    @contacts = []
+  end
+  
+  def print_results(search, results)
+    puts search
+    results.each do |contact|
+      puts contact.to_s('full_name')
+      contact.print_phone_numbers
+      contact.print_addresses
+      puts "\n"
     end
+  end
+  
+  def find_by_name(name)
+    results = []
+    search = name.downcase
+    contacts.each do |contact|
+      if contact.full_name.downcase.include?(search)
+        results.push(contact)
+      end
+    end
+    print_results("Name search results (#{search})", results)
+  end
+  
+  def find_by_phone_number(number)
+    results = []
+    search = number.gsub("-", "")
+    contacts.each do |contact|
+      contact.phone_numbers.each do |phone_number|
+        if phone_number.number.gsub("-", "").include?(search)
+          results.push(contact) unless results.include?(contact)
+        end
+      end
+    end
+    print_results("Phone search results (#{search})", results)
+  end
 
-    def print_contact_list
-        puts "Contact list"
-        contacts.each do |contact|
-            puts contact.to_s('last_first')
+  def find_by_address(query) 
+    results = []
+    search = query.downcase
+    contacts,.each do |contact|
+        contact.addresses.each do |address|
+            if address.to_s('long').downcase.include?(search)
+                results.push(contact) unless results.include?(contact)
+            end
         end
     end
+    print_results("Address search results (#{search})", results)
+  end
+  
+  def print_contact_list
+    puts "Contact List"
+    contacts.each do |contact|
+      puts contact.to_s('last_first')
+    end
+  end
 end
 
 address_book = AddressBook.new
 
-ryan = Contact.new
-ryan.first_name = "Ryan"
-ryan.last_name = "Webb"
-ryan.add_phone_number("Home", "123-456-7890")
-ryan.add_phone_number("Work", "435-456-7890")
-ryan.add_address("Home", "123 Main St.", "", "Portland", "OR", "12345")
+jason = Contact.new
+jason.first_name = "Jason"
+jason.last_name = "Seifer"
+jason.add_phone_number("Home", "123-456-7890")
+jason.add_phone_number("Work", "456-789-0123")
+jason.add_address("Home", "123 Main St.", "", "Portland", "OR", "12345")
 
-address_book.contacts.push(ryan)
-address_book.print_contact_list
+nick = Contact.new
+nick.first_name = "Nick"
+nick.last_name = "Pettit"
+nick.add_phone_number("Home", "222-222-2222")
+nick.add_address("Home", "222 Two Lane", "", "Portland", "OR", "12345")
 
+address_book.contacts.push(jason)
+address_book.contacts.push(nick)
+
+#address_book.print_contact_list
+
+address_book.find_by_name("e")
+address_book.find_by_phone_number("2")
+address_book.find_by_address("two")
